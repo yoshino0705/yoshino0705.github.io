cee10edebc29ee9fda6c324e115f9498ede8d197
yoshino0705 <weihaomouse@gmail.com>
2020-08-01 20:22:03 -0400

    Updates

---
+++ precache-manifest.12049924a06a21b7d33ed38cd59509b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a75ea2a4f8d5d0081fb08a656c400d7",
+    "url": "/yoshinobhl-site/index.html"
+  },
+  {
+    "revision": "43b54c0aef1697198640",
+    "url": "/yoshinobhl-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f7817d59777d3f585c0",
+    "url": "/yoshinobhl-site/static/js/2.970f41f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yoshinobhl-site/static/js/2.970f41f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b54c0aef1697198640",
+    "url": "/yoshinobhl-site/static/js/main.5c92d6e9.chunk.js"
+  },
+  {
+    "revision": "e1c9f54bccaadd36e523",
+    "url": "/yoshinobhl-site/static/js/runtime-main.494352fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yoshinobhl-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
