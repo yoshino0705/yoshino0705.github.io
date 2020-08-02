1614bca1e18018d90e09fe91f182e90359b611ac
yoshino0705 <weihaomouse@gmail.com>
2020-08-01 21:18:38 -0400

    Updates

---
+++ precache-manifest.0f77192961b88971c423c56695dfec24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb00182940033b40c22d847fe8ac3d0",
+    "url": "/yoshinobhl-site/index.html"
+  },
+  {
+    "revision": "93421ac54f1179883100",
+    "url": "/yoshinobhl-site/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6df861e628fb3d0529c9",
+    "url": "/yoshinobhl-site/static/js/2.d2e3b06c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yoshinobhl-site/static/js/2.d2e3b06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93421ac54f1179883100",
+    "url": "/yoshinobhl-site/static/js/main.ba387cff.chunk.js"
+  },
+  {
+    "revision": "e1c9f54bccaadd36e523",
+    "url": "/yoshinobhl-site/static/js/runtime-main.494352fc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yoshinobhl-site/precache-manifest.5f113e2136cdf2c3c8bf43ff901f1c90.js"
+  "/yoshinobhl-site/precache-manifest.0f77192961b88971c423c56695dfec24.js"
 );
 
 self.addEventListener('message', (event) => {
