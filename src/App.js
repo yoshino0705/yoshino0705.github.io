7ab6ed96c0ba68a465f7f79dcde9996be425bc02
yoshino0705 <weihaomouse@gmail.com>
2020-08-01 21:22:55 -0400

    ga and helmet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Helmet } from "react-helmet"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import GA from './utils/GoogleAnalytics'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Helmet>
+          <meta name="google-site-verification" content="lxJ1q0JkfRn2i_1vXkwQlVQTArUQ-B8ACaxLnvyQt34" />
+        </Helmet>
+        {GA.init() && <GA.RouteTracker />}
+        <Switch>
+          <Route path='/auth' component={<div />} />
+          <Route path='/error' component={<div />} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
