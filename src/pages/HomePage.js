465bb1a93d67652182c37a2c82ee36beda6b4c27
yoshino0705 <weihaomouse@gmail.com>
2020-08-07 01:36:13 -0400

    home page content

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from 'react'
+import { useSelector } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
 import Page from '../components/Page'
 import friedshrimp from '../components/assets/friedshrimp.png'
+
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+import { HOME_PAGE_CONTENT } from './constants'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,22 +18,130 @@ const useStyles = makeStyles((theme) => ({
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     backgroundImage: `url(${friedshrimp})`,
-    height: '150vh'
+    height: '150vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-
+  container: {
+    padding: theme.spacing(3)
+  },
+  card: {
+    backgroundColor: 'rgba(223, 169, 169, 0.70)',
+    borderRadius: 12,
+    width: 600
+  },
+  card2: {
+    backgroundColor: 'rgba(38, 242, 242, 0.70)',
+    borderRadius: 12,
+    width: 600
+  },
+  family: {
+    fontWeight: 800,
+    color: '#FF6200'
+  },
+  text: {
+    color: '#A63F00'
+  },
+  intro: {
+    fontSize: 20
+  },
+  subText: {
+    fontSize: 16
+  },
+  werewolf: {
+    fontSize: 34
+  }
 }));
 
 const HomePage = () => {
 
   const classes = useStyles()
+  const language = useSelector(state => state.language)
+  const content = HOME_PAGE_CONTENT[language.locale]
 
   return (
     <Page
       className={classes.root}
+      elevation={24}
       title="Home"
     >
+      <Card
+        className={classes.card}
+      >
+        <CardContent>
+          <Grid
+            className={classes.container}
+            container
+            direction="column"
+            spacing={2}
+          >
+            <Grid item>
+              <Typography
+                className={classes.text}
+                variant="h4"
+              >
+                {content.welcome}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                className={classes.family}
+                variant="h1"
+              >
+                {content.koumi}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                className={classes.text}
+                variant="h4"
+              >
+                {content.website}
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
 
-      Home Page
+      <Card
+        className={classes.card2}
+      >
+        <CardContent>
+          <Grid
+            className={classes.container}
+            container
+            direction="column"
+            spacing={2}
+          >
+            <Grid item>
+              <Typography
+                className={classes.intro}
+                variant="h4"
+              >
+                {content.introduction}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                className={classes.werewolf}
+                variant="h1"
+              >
+                {content.werewolf}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                className={classes.subText}
+                variant="h4"
+              >
+                ({content.werewolf_note})
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </Page>
   )
 }
---
+++ src/pages/constants.js
@@ -0,0 +1,27 @@
+export const LANGUAGES = {
+  'zh-TW': '中文',
+  'en-US': 'English'
+}
+
+const ZH_TW = {
+  koumi: '酷米家族',
+  welcome: '歡迎來到',
+  website: '的網站',
+  introduction: '我們原先是一個聊天群(現在也是)，某一天有人突然說想要做做看Minecraft地圖，群裡又剛好有人會指令和蓋地圖，因而誕生了我們團隊的最初作品：',
+  werewolf: '人狼殺',
+  werewolf_note: '其實就是狼人殺，可是當時我們比較習慣叫他人狼'
+}
+
+const EN_US = {
+  koumi: 'The Koumi Family\'s',
+  welcome: 'Welcome to',
+  website: 'Website',
+  introduction: 'We met each other on an online chat group, one day out of nowhere someone proposed the idea of making a Minecraft multiplayer map, which turned out that we also had members who were familiar with building and commands, and this is how our first ever creation was born:',
+  werewolf: 'The Werewolf Game',
+  werewolf_note: 'Similar to Town of Salem, but with different roles, abilities, and playstyle deviations'
+}
+
+export const HOME_PAGE_CONTENT = {
+  'zh-TW': ZH_TW,
+  'en-US': EN_US
+}
